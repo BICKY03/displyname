fd3ee63546a2fa1911bddd8ecaaad15413de9d02
BICKY03 <jotirajmajhi29@gmail.com>
2024-05-19 01:29:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const[name,setName]=useState({firstName:"",lastName:""})
+  const [fullName, setFullName] = useState('');
+
+  function handleChange(event){
+    const{name,value}=event.target;
+    setName((prev)=>({
+      ...prev,
+      [name]:value
+    }))
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    if (name.firstName && name.lastName) {
+      setFullName(`${name.firstName} ${name.lastName}`);
+    }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your firstName
+          <input type="text" name="firstName" value={name.firstName} placeholder="Enter your firstName" onChange={handleChange}
+          required/>
+        </label>
+
+        <label>
+          Enter your lastNamtName
+          <input type="text" name="lastName" value={name.lastName} placeholder="Enter your lastName" onChange={handleChange}
+          required/>
+        </label>
+        <button type="submit" >Submit</button>
+      </form>
+      {fullName && <p>Your full name is: {fullName}</p>}
+       
     </div>
   );
 }
